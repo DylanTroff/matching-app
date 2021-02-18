09a0b0ba324719a9ea82b7db98faadf79ef3e42e
DylanTroff <70645469+DylanTroff@users.noreply.github.com>
2021-02-18 11:12:03 +0100

    Add files via upload

---
+++ scriptfe.js
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName ('navbar-links')[0]
+
+toggleButton.addEventListener('click', klikken)
+
+function klikken(){
+    navbarLinks.classList.toggle('active')
+    console.log("er is geklikt")
+}
