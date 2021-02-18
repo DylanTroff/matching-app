535dcc8804f0acaee323daf43d8e163546a5f55a
DylanTroff <70645469+DylanTroff@users.noreply.github.com>
2021-02-18 11:12:36 +0100

    Delete scriptfe.js

--- scriptfe.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName ('navbar-links')[0]
-
-toggleButton.addEventListener('click', klikken)
-
-function klikken(){
-    navbarLinks.classList.toggle('active')
-    console.log("er is geklikt")
-}
