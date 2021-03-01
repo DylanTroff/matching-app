f514b15bfedd0155bb8ba5492e65275f398b23e9
DylanTroff <dtroff@upcmail.nl>
2021-03-01 17:27:18 +0100

    dymnamic data

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.engine('hbs', exphbs());
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs({
+  defaultLayout:'main',
+  layoutsDir:'views/layouts',
+}),
+);
+app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
+  res.render('home', {title:'Home', profiel})
+});
 app.get('/about', (req, res) => {
     res.render('about')
-  })
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
 
 app.use(express.static('public'));
+
+const profiel = [{
+  name:"Arie",
+  age: "23"
+}]
